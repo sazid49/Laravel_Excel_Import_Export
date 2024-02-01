7eb0b4b9e545ee23b26eb3211823ad42bacae3b1
sazid49 <sazidfahim49@gmail.com>
2024-02-02 00:03:26 +0600

    with heading

--- app/Exports/ExportUser.php
+++ app/Exports/ExportUser.php
@@ -3,15 +3,22 @@
 namespace App\Exports;
 
 use App\Models\User;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class ExportUser implements FromCollection
-{
+class ExportUser implements FromCollection,WithHeadings
+{   
+    use Exportable;
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
         return User::select('name','email')->get();
+    }
+    public function headings(): array
+    {
+        return array_keys($this->collection()->first()->toArray());
     }
 }
